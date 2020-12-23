c57a424c86d2c2e06e4adfd73eb6984af853e7ad
a-abdulrahman <ali.abdulrahman@t-online.de>
2020-12-23 13:00:53 +0100

    Function.js new function

--- dimensions/function.js
+++ dimensions/function.js
@@ -85,4 +85,97 @@ function getTime(startDate, endDate, from, to, Model) {
     })
   }
 
-  module.exports = {getTime};
+  function getBusyTimes(startDate, endDate, from, to, Model){
+        return new Promise(function(resolve, reject){
+            Model.aggregate([
+                {
+                    $match: {
+                        $and: [
+                            {
+                                createdAt: {
+                                    $gt: new Date(startDate)
+                                }
+                            }, {
+                                createdAt: {
+                                    $lt: new Date(endDate)
+                                }
+                            }
+                        ]
+                    }
+                },{
+                  '$match': {
+                    '$or': [
+                      {
+                        '$and': [
+                          {
+                            'data.action': 'started'
+                          }, {
+                            'type': 'videoChanged'
+                          }
+                        ]
+                      }, {
+                        '$and': [
+                          {
+                            'data.action': 'started'
+                          }, {
+                            'type': 'screenSharingChanged'
+                          }
+                        ]
+                      }, {
+                        '$and': [
+                          {
+                            'data.action': 'started'
+                          }, {
+                            'type': 'coBrowsingChanged'
+                          }
+                        ]
+                      }
+                    ]
+                  }
+                }, {
+                  '$project': {
+                    '_id': 0, 
+                    'type': 1, 
+                    'hours': {
+                      '$hour': '$createdAt'
+                    }, 
+                    'datetime': {
+                      '$dateToString': {
+                        'format': '%H:%M:%S', 
+                        'date': '$createdAt'
+                      }
+                    }
+                  }
+                }, {
+                  '$match': {
+                    '$and': [
+                      {
+                        'hours': {
+                          '$gt': from
+                        }
+                      }, {
+                        'hours': {
+                          '$lt': to
+                        }
+                      }
+                    ]
+                  }
+                }, {
+                  '$group': {
+                    '_id': '$hours', 
+                    'count': {
+                      '$sum': 1
+                    }
+                  }
+                }, {
+                  '$sort': {
+                    '_id': 1
+                  }
+                }
+              ], function(err, result){
+                  resolve(result)
+              })
+        })
+  }
+
+  module.exports = {getTime, getBusyTimes};
